b53c3c7a528f48508cb2ce752112d4943a9bff97
YoshikiOno729 <ono.yoshiki.eo@mynavi.jp>
2024-05-22 05:04:59 +0000

    modified prime_ono

--- Ono/prime_ono.py
+++ Ono/prime_ono.py
@@ -3,13 +3,19 @@ args = sys.argv
 num = int(args[1])
 
 #通常の処理
-if num < 1000:
-    for i in range(1,1000,1):
+if num == 1 or num == 2:
+    print("Prime")
+
+elif 2 < num < 1000:
+    for i in range(2, num - 1, 1):
         if num % i == 0:
-            print("Prime")
+            print("not")
             break
+        else:
+            pass
     else:
-        print("not")
+        print("Prime")
+
 #1000以上が入力された場合の処理
 else:
     print("1000以上は判定できません")
