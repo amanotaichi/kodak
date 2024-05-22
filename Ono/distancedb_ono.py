39de4971290305893690295e04e2c53cbc6d014e
YoshikiOno729 <ono.yoshiki.eo@mynavi.jp>
2024-05-22 06:52:40 +0000

    iroiro

---
+++ Ono/distancedb_ono.py
@@ -0,0 +1,4 @@
+import sys
+args = sys.argv
+from_station = int(args[1])
+to_station = int(args[2])
---
+++ Ono/stationstable.sql
@@ -0,0 +1,14 @@
+INSERT INTO stations VALUES(1, "東京", 0.00),
+(2, "品川", 6.78),
+(3, "新横浜", 25.54),
+(4, "名古屋", 342.02),
+(5, "京都", 476.31),
+(6, "新大阪", 515.35);
+
+SHOW database;
+
+CREATE TABLE stations(
+    "seq" INT PRIMARY KEY,
+    "name" VARCHAR(20),
+    "kilo"  DECIMAL(6,2)
+)
