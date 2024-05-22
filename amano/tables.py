2de95a48724eca676e19db91c104e1ee110a1d17
amanotaichi <amano.taichi.ix@mynavi.jp>
2024-05-22 08:16:38 +0000

    add distancedb.py

---
+++ amano/tables.py
@@ -0,0 +1,20 @@
+import sys
+from sqlalchemy import Column, String, Date, Integer, Numeric, DateTime
+from databases import Base
+from databases import ENGINE
+
+#テーブル：Stationsの定義
+class Stations(Base):
+    __tablename__ = 'stations'
+    seq = Column('seq', Integer, primary_key = True)
+    name = Column('name', String(20))
+    kilo = Column('kilo', Numeric(6, 2))
+
+def main(args):
+    """
+    メイン関数
+    """
+    Base.metadata.create_all(bind=ENGINE)
+
+if __name__ == "__main__":
+        main(sys.argv)
